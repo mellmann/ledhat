996bd0037562fb00658d7b300169c8662f4307be
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2023-11-16 01:17:27 +0100

    cleanup

--- hat-matrix-fire/hat-matrix-fire.ino
+++ hat-matrix-fire/hat-matrix-fire.ino
@@ -6,6 +6,7 @@
 
 #include <ezButton.h>  // the library to use for SW pin
 #include "Encoder.h"
+
 #include <ESP32Encoder.h>
 
 #include <Adafruit_BNO055.h>
@@ -25,22 +26,20 @@ Adafruit_BNO055 bno;
 #include <FastLED.h>
 // Here's how to control the LEDs from any two pins:
 #define DATAPIN    13
+#define NUMPIXELS 600 // Number of LEDs in strip
+
+CRGB leds[ NUMPIXELS ];
 
 
 const uint8_t kMatrixWidth = 69;
 const uint8_t kMatrixHeight = 18;
-#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
-#define LAST_VISIBLE_LED 1000
-
-CRGB leds[ NUM_LEDS ];
-
-uint16_t XYTable[NUM_LEDS];
+uint16_t XYTable[kMatrixWidth * kMatrixHeight];
 
 uint16_t XY( uint8_t x, uint8_t y)
 {
   // any out of bounds address maps to the first hidden pixel
   if ( (x >= kMatrixWidth) || (y >= kMatrixHeight) ) {
-    return (LAST_VISIBLE_LED + 1);
+    return NUMPIXELS;
   }
 
   uint16_t i = (y * kMatrixWidth) + x;
@@ -53,8 +52,8 @@ void setup()
 {  
 
   // initialize the matrix values
-  for(int i = 0; i < NUM_LEDS; ++i) {
-    XYTable[i] = (i % 2 == 0) ? i / 2 : LAST_VISIBLE_LED;
+  for(int i = 0; i < kMatrixWidth * kMatrixHeight; ++i) {
+    XYTable[i] = (i % 2 == 0) ? i / 2 : NUMPIXELS;
   }
   
   //encoder.attachFullQuad ( DT_PIN, CLK_PIN );
@@ -75,7 +74,7 @@ void setup()
 
   button.setDebounceTime(50);  // set debounce time to 50 milliseconds
 
-  FastLED.addLeds<WS2812B, DATAPIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, DATAPIN, GRB>(leds, NUMPIXELS);
   FastLED.setBrightness(64);
   FastLED.setDither(0);
 }
@@ -137,16 +136,8 @@ void loop()
 
   FastLED.clear();
 
-  //leds[XY(10,counter)] = HeatColor(60);
-  
   Fire2012();
 
-  /*
-  for( int x = 0; x < kMatrixWidth; ++x) {
-    leds[XY(x,counter)] = HeatColor(60);
-  }
-  */
-  
 
   //counter = (counter + 1) % kMatrixHeight;
   FastLED.show();
