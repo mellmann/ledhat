a6b8f9e985a48e3f0a4834e14f971b1964c1b12b
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2023-11-17 23:43:10 +0100

    structured into classes

--- hat-matrix-fire/hat-matrix-fire.ino
+++ hat-matrix-fire/hat-matrix-fire.ino
@@ -23,47 +23,95 @@ ezButton button(SW_PIN);
 
 Adafruit_BNO055 bno;
 
+
 #include <FastLED.h>
 // Here's how to control the LEDs from any two pins:
 #define DATAPIN    13
-#define NUMPIXELS 600 // Number of LEDs in strip
 
-// the last pixel is invalid
-CRGB leds[ NUMPIXELS + 1 ];
+#define NUMPIXELS     600 // number of LEDs in strip
+#define MATRIX_WIDTH  69  // number of pixes in a line of the virtual matrix
+#define MATRIX_HEIGHT 18  // number of line in the virtual matrix
 
-// define matrix mapping to leds
-const uint8_t kMatrixWidth  = 69;
-const uint8_t kMatrixHeight = 18;
-
-// maps y*kMatrixWidth + x to LED-idx
-uint16_t xy2idx[kMatrixWidth * kMatrixHeight];
-
-uint16_t XY(uint8_t x, uint8_t y)
+/*
+ * // this might make the code more elegant, but also slower because of virtual functions
+class SimplePixelMatrix 
 {
-  // any out of bounds address maps to the first hidden pixel
-  if ( (x >= kMatrixWidth) || (y >= kMatrixHeight) ) {
-    return NUMPIXELS;
+public:
+  virtual int width() = 0;
+  virtual int height() = 0;
+  virtual CRGB& pixel(uint8_t x, uint8_t y) = 0;
+  virtual void set_pixel(uint8_t x, uint8_t y, const CRGB& color) = 0;
+}
+*/
+
+
+template<int NUM_PIXELS, int WIDTH, int HEIGHT>
+class HatMatrix
+{
+public:
+  HatMatrix()
+  {
+    // initialize the mapping of the matrix values to the led strip
+    for(int i = 0; i < WIDTH * HEIGHT; ++i) {
+      xy2idx[i] = (i % 2 == 0) ? min(i / 2, NUM_PIXELS) : NUM_PIXELS;
+    }
   }
 
-  return xy2idx[y * kMatrixWidth + x];
-}
+  inline CRGB& pixel(uint8_t x, uint8_t y) {
+    return leds[XY(x,y)];
+  }
+  
+  inline void set_pixel(uint8_t x, uint8_t y, const CRGB& color) {
+    leds[XY(x,y)] = color;
+  }
 
-CRGB& pixel(uint8_t x, uint8_t y) {
-  return leds[XY(x,y)];
-}
+  inline CRGB* getLEDArray() {
+    return leds;
+  }
 
-void set_pixel(uint8_t x, uint8_t y, const CRGB& color) {
-  leds[XY(x,y)] = color;
-}
+  inline int num_pixels() const {
+    return NUM_PIXELS;
+  }
+
+  inline int width() const {
+    return WIDTH;
+  }
+  
+  inline int height() const {
+    return HEIGHT;
+  }
+
+private:
+  uint16_t XY(uint8_t x, uint8_t y)
+  {
+    // any out of bounds address maps to the first hidden pixel
+    if ( (x >= WIDTH) || (y >= HEIGHT) ) {
+      return NUM_PIXELS;
+    }
+  
+    return xy2idx[y * WIDTH + x];
+  }
+
+public:
+  static const uint8_t Width  = WIDTH;
+  static const uint8_t Height = HEIGHT;
+  static const int Num_Pixels = NUM_PIXELS;
+
+private:
+  // the last pixel is invalid
+  CRGB leds[ NUM_PIXELS + 1 ];
+
+  // maps y*kMatrixWidth + x to LED-idx
+  uint16_t xy2idx[WIDTH * HEIGHT];
+};
+
+typedef HatMatrix<NUMPIXELS, MATRIX_WIDTH, MATRIX_HEIGHT> MyHatMatrix;
+MyHatMatrix matrix;
+
+
 
 void setup() 
 {  
-
-  // initialize the mapping of the matrix values to the led strip
-  for(int i = 0; i < kMatrixWidth * kMatrixHeight; ++i) {
-    xy2idx[i] = (i % 2 == 0) ? min(i / 2, NUMPIXELS) : NUMPIXELS;
-  }
-  
   //encoder.attachFullQuad ( DT_PIN, CLK_PIN );
   encoder.attachSingleEdge ( DT_PIN, CLK_PIN );
   encoder.setCount ( 0 );
@@ -81,68 +129,84 @@ void setup()
   
   button.setDebounceTime(50);  // set debounce time to 50 milliseconds
 
-  FastLED.addLeds<WS2812B, DATAPIN, GRB>(leds, NUMPIXELS);
+  // register the LED array
+  FastLED.addLeds<WS2812B, DATAPIN, GRB>(matrix.getLEDArray(), matrix.num_pixels());
   FastLED.setBrightness(64);
   FastLED.setDither(0);
 }
 
-int COOLING_BASE = 100;
-int SPARKING_BASE = 120;
-
-// array of temperature readings at each simulation cell
-uint8_t heat[kMatrixWidth][kMatrixHeight];
-
-void fire2012(int counter)
+template<class MATRIX_TYPE>
+class Fire
 {
-  int COOLING  = COOLING_BASE  + counter*10;
-  int SPARKING = SPARKING_BASE - counter*10;
-  
-  // Step 1.  Cool down every cell a little
-  for( int x = 0; x < kMatrixWidth; ++x) {
-    for( int y = 0; y < kMatrixHeight; ++y) {
-      heat[x][y] = qsub8( heat[x][y],  random8(0, ((COOLING * 10) / kMatrixHeight) + 2));
-    }
-  }
+public:
+  Fire(MATRIX_TYPE& matrix) : matrix(matrix) {}
+
+  void update(int factor) 
+  {
+    int COOLING  = cooling_base  + factor*10;
+    int SPARKING = sparkling_base - factor*10;
     
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for( int x = 0; x < kMatrixWidth; ++x) {
-    for( int y = kMatrixHeight-1; y >= 2; y--) {
-      heat[x][y] = (heat[x][y - 1] + heat[x][y - 2] + heat[x][y - 2] ) / 3;
-    } 
-  }
+    // Step 1.  Cool down every cell a little
+    for( int x = 0; x < matrix.width(); ++x) {
+      for( int y = 0; y < matrix.height(); ++y) {
+        heat[x][y] = qsub8( heat[x][y],  random8(0, ((COOLING * 10) / matrix.height()) + 2));
+      }
+    }
+      
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int x = 0; x < matrix.width(); ++x) {
+      for( int y = matrix.height()-1; y >= 2; y--) {
+        heat[x][y] = (heat[x][y - 1] + heat[x][y - 2] + heat[x][y - 2] ) / 3;
+      } 
+    }
+    
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    for( int x = 0; x < matrix.width(); ++x) {
+      if( random8() < SPARKING ) {
+        int y = random8(2);
+        heat[x][y] = qadd8( heat[x][y], random8(160,255) );
+      }
+    }
   
-  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-  for( int x = 0; x < kMatrixWidth; ++x) {
-    if( random8() < SPARKING ) {
-      int y = random8(2);
-      heat[x][y] = qadd8( heat[x][y], random8(160,255) );
+    // Step 4.  Map from heat cells to LED colors
+    for( int x = 0; x < matrix.width(); ++x) {
+      for( int y = 0; y < matrix.height(); ++y) {
+        //matrix.set_pixel(x, (matrix.height()-1) - y, HeatColor( heat[x][y]));
+        matrix.pixel(x, (matrix.height()-1) - y) = HeatColor( heat[x][y]);
+      }
     }
   }
 
-  // Step 4.  Map from heat cells to LED colors
-  for( int x = 0; x < kMatrixWidth; ++x) {
-    for( int y = 0; y < kMatrixHeight; ++y) {
-      //set_pixel(x, (kMatrixHeight-1) - y, HeatColor( heat[x][y]));
-      pixel(x, (kMatrixHeight-1) - y) = HeatColor( heat[x][y]);
-    }
-  }
-}
+private:
+  MATRIX_TYPE& matrix;
+  
+  int cooling_base   = 100;
+  int sparkling_base = 120;
+  
+  // array of temperature readings at each simulation cell
+  uint8_t heat[MATRIX_TYPE::Width][MATRIX_TYPE::Height];
+};
+
+Fire<MyHatMatrix> fire(matrix);
+
+
 
 void loop() 
 {
+  const int maxCounter = 10;
   int counter = encoder.getCount();
   if(counter < 0) {
     counter = 0;
     encoder.setCount(counter);
-  } else if(counter >= kMatrixHeight) {
-    counter = kMatrixHeight;
+  } else if(counter >= maxCounter) {
+    counter = maxCounter;
     encoder.setCount(counter);
   }
-  Serial.println((int)counter);
+  //Serial.println(counter);
 
   FastLED.clear();
 
-  fire2012(counter);
+  fire.update(counter);
 
   //counter = (counter + 1) % kMatrixHeight;
   FastLED.show();
